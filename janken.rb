7c7d73c15c1fa999e7f371aef21591dcef3e5f4e
ryuuya <ryuuya0921@gmail.com>
2024-02-01 06:33:51 +0900

    完成

--- janken.rb
+++ janken.rb
@@ -1,3 +1,32 @@
-puts "HelloWolld"
 
-puts "OK"
+
+  puts "じゃんけん..."
+  puts "0(グー)1(チョキ)2'(パー)3(戦わない)"
+
+  janken_type = gets.to_i
+
+  while janken_type > 3
+    puts "入力が間違っています"
+    puts "0(グー)1(チョキ)2'(パー)3(戦わない)"
+    janken_type = gets.chomp.to_i
+    #他の数字を選択した場合に再度質問する
+  end
+
+  hand_type = ["グー","チョキ","パー"]
+  my_hand = janken_type
+  pc_hand = rand(0..2)
+
+if janken_type < 3
+
+  hand_type = ["グー", "チョキ", "パー"]
+  my_hand = janken_type
+  pc_hand = rand(0..2)
+
+  puts "あなたは#{hand_type[my_hand]}です。"
+  puts "コンピュータは#{hand_type[pc_hand]}です。"
+
+  puts "ーーーーーーーーーーーーーーーーーーー"
+
+else
+  puts "わかりました。Ctrl + D で終了してください"
+end
