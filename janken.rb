f0b82c5851c7f4a6a648336a02b6c3feeb0e33a2
ryuuya <ryuuya0921@gmail.com>
2024-02-04 06:05:38 +0900

    正解の処理

--- janken.rb
+++ janken.rb
@@ -1,73 +1,61 @@
-
-
-puts "じゃんけん..."
-puts "0(グー)1(チョキ)2'(パー)3(戦わない)"
-
 loop do
+  puts "じゃんけん..."
+  puts "0(グー)1(チョキ)2(パー)3(戦わない)"
 
-@janken_type = gets.to_i
+  janken_type = gets.to_i
 
-puts "ホイ!"
-puts "ーーーーーーーーーーーーーーーーーーー"
+  puts "ホイ!"
+  puts "ーーーーーーーーーーーーーーーーーーー"
 
-while @janken_type > 3
-  puts "入力が間違っています"
-  puts "0(グー)1(チョキ)2'(パー)3(戦わない)"
-  @janken_type = gets.chomp.to_i
-  #他の数字を選択した場合に再度質問する
-end
+  while janken_type > 3
+    puts "入力が間違っています"
+    puts "0(グー)1(チョキ)2(パー)3(戦わない)"
+    janken_type = gets.chomp.to_i
+    #他の数字を選択した場合に再度質問する
+  end
 
-if @janken_type == 3
-  puts "じゃんけんを終了します"
-  break
-end
-
-hand_type = ["グー","チョキ","パー"]
-my_hand = @janken_type
-pc_hand = rand(0..2) #ランダムで出す
-
-puts "あなた：#{hand_type[my_hand]}を出しました。"
-puts "相手：#{hand_type[pc_hand]}を出しました。"
-
-puts "ーーーーーーーーーーーーーーーーーーー"
-
-if my_hand == pc_hand
-  puts "あいこなので、もう一度手を選んでください"
-  puts "0(グー)1(チョキ)2'(パー)3(戦わない)"
-else
-  break
-end
-end
-
-# ーーーーーあっち向いてほいの処理ーーーーーーー
-
-loop do
-  if @janken_type != 3
-    puts "あっち向いて"
-    puts "0(上)1(下)2(左)3(右)"
-
-    point_type = gets.to_i
-
-    puts "ショ！"
-
-    finger_type = ["上","下","左","右"]
-    i_hand = point_type
-    yo_hand = rand(0..3) #ランダムに出す
-
-    puts "あなた：#{finger_type[i_hand]}"
-    puts "相手：#{finger_type[yo_hand]}"
-
-    puts "ーーーーーーーーーーーーーーーーーーー"
-
-    if i_hand == yo_hand
-      puts "あなたの勝ちです！"
-      break  # 勝った場合、ループを抜ける
-    else
-      puts "あなたの負けです..."
-    end
-  else
+  if janken_type == 3
     puts "じゃんけんを終了します"
     break  # じゃんけんを終了する場合、ループを抜ける
   end
-end
 
+  hand_type = ["グー","チョキ","パー"]
+  my_hand = janken_type
+  pc_hand = rand(0..2) #ランダムで出す
+
+  puts "あなた：#{hand_type[my_hand]}を出しました。"
+  puts "相手：#{hand_type[pc_hand]}を出しました。"
+
+  puts "ーーーーーーーーーーーーーーーーーーー"
+
+  if my_hand == pc_hand
+    puts "あいこです。もう一度じゃんけんを行います。"
+    next  # あいこの場合、ループを続けて再度じゃんけんを行う
+  end
+
+  # じゃんけんの勝敗が決まった場合、あっち向いてホイの処理へ移る
+  puts "あっち向いて"
+  puts "0(上)1(下)2(左)3(右)"
+
+  point_type = gets.to_i
+
+  puts "ショ！"
+
+  finger_type = ["上","下","左","右"]
+  i_hand = point_type
+  yo_hand = rand(0..3) #ランダムに出す
+
+  puts "あなた：#{finger_type[i_hand]}"
+  puts "相手：#{finger_type[yo_hand]}"
+
+  puts "ーーーーーーーーーーーーーーーーーーー"
+
+  if i_hand == yo_hand
+    puts "あなたの勝ちです！"
+    break  # 勝った場合、ループを抜ける
+  else
+    puts "あなたの負けです..."
+  end
+
+  # じゃんけんの結果が決まらない場合、再度じゃんけんを行うためループを継続
+end
