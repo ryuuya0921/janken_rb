86a9dc74df96da2e729d0ff9d1362d9a89551780
ryuuya <ryuuya0921@gmail.com>
2024-02-04 06:26:08 +0900

    修正

--- janken.rb
+++ janken.rb
@@ -3,40 +3,44 @@
 puts "じゃんけん..."
 puts "0(グー)1(チョキ)2'(パー)3(戦わない)"
 
+@janken_type = nil # @janken_type を初期化
+
 loop do
 
-@janken_type = gets.to_i
+  @janken_type = gets.to_i
 
-puts "ホイ!"
-puts "ーーーーーーーーーーーーーーーーーーー"
+  puts "ホイ!"
+  puts "ーーーーーーーーーーーーーーーーーーー"
 
-while @janken_type > 3
-  puts "入力が間違っています"
-  puts "0(グー)1(チョキ)2'(パー)3(戦わない)"
-  @janken_type = gets.chomp.to_i
-  #他の数字を選択した場合に再度質問する
-end
+  while @janken_type > 3
+    puts "入力が間違っています"
+    puts "0(グー)1(チョキ)2'(パー)3(戦わない)"
+    @janken_type = gets.chomp.to_i
+    #他の数字を選択した場合に再度質問する
+  end
 
-if @janken_type == 3
-  puts "じゃんけんを終了します"
-  break
-end
+  if @janken_type == 3
+    puts "じゃんけんを終了します"
+    break
+  end
 
-hand_type = ["グー","チョキ","パー"]
-my_hand = @janken_type
-pc_hand = rand(0..2) #ランダムで出す
+  hand_type = ["グー","チョキ","パー"]
+  my_hand = @janken_type
+  pc_hand = rand(0..2) #ランダムで出す
 
-puts "あなた：#{hand_type[my_hand]}を出しました。"
-puts "相手：#{hand_type[pc_hand]}を出しました。"
+  puts "あなた：#{hand_type[my_hand]}を出しました。"
+  puts "相手：#{hand_type[pc_hand]}を出しました。"
 
-puts "ーーーーーーーーーーーーーーーーーーー"
+  puts "ーーーーーーーーーーーーーーーーーーー"
 
-if my_hand == pc_hand
-  puts "あいこなので、もう一度手を選んでください"
-  puts "0(グー)1(チョキ)2'(パー)3(戦わない)"
-else
-  break
-end
+  if my_hand == pc_hand
+    puts "あいこなので、もう一度手を選んでください"
+    puts "0(グー)1(チョキ)2'(パー)3(戦わない)"
+    next
+    
+  else
+    break
+  end
 end
 
 # ーーーーーあっち向いてほいの処理ーーーーーーー
@@ -61,9 +65,13 @@ if @janken_type != 3
 
   if i_hand == yo_hand #勝敗が決まるまで繰り返し行う処理
     break
+  else
+    puts "再度じゃんけんをします"
+    puts "0(グー)1(チョキ)2'(パー)3(戦わない)"
+    break
   end
 else
   puts "終了します"
-  break
-end
+    break
+  end
 end
