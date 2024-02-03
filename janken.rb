ab953f94b05632cc241ff96d9b8a3f6784331fa5
ryuuya <ryuuya0921@gmail.com>
2024-02-03 20:12:04 +0900

    参考

--- janken.rb
+++ janken.rb
@@ -1,34 +1,73 @@
+
+
 puts "じゃんけん..."
-puts "0(グー) 1(チョキ) 2(パー) 3(戦わない)"
+puts "0(グー)1(チョキ)2'(パー)3(戦わない)"
 
 loop do
-  janken_type = gets.to_i
 
-  while janken_type > 3
-    puts "入力が間違っています"
-    puts "0(グー) 1(チョキ) 2(パー) 3(戦わない)"
-    janken_type = gets.to_i
-  end
+@janken_type = gets.to_i
 
-  if janken_type == 3
-    puts "じゃんけんを終了します"
-    break
-  end
+puts "ホイ!"
+puts "ーーーーーーーーーーーーーーーーーーー"
 
-  hand_type = ["グー", "チョキ", "パー"]
-  my_hand = janken_type
-  pc_hand = rand(0..2)
+while @janken_type > 3
+  puts "入力が間違っています"
+  puts "0(グー)1(チョキ)2'(パー)3(戦わない)"
+  @janken_type = gets.chomp.to_i
+  #他の数字を選択した場合に再度質問する
+end
 
-  puts "あなた：#{hand_type[my_hand]}を出しました。"
-  puts "相手：#{hand_type[pc_hand]}を出しました。"
+if @janken_type == 3
+  puts "じゃんけんを終了します"
+  break
+end
 
-  puts "ーーーーーーーーーーーーーーーーーーー"
+hand_type = ["グー","チョキ","パー"]
+my_hand = @janken_type
+pc_hand = rand(0..2) #ランダムで出す
 
-  if my_hand == pc_hand
-    puts "あいこなので、もう一度じゃんけんしてください"
-    puts "じゃんけん..."
-    puts "0(グー) 1(チョキ) 2(パー) 3(戦わない)"
+puts "あなた：#{hand_type[my_hand]}を出しました。"
+puts "相手：#{hand_type[pc_hand]}を出しました。"
+
+puts "ーーーーーーーーーーーーーーーーーーー"
+
+if my_hand == pc_hand
+  puts "あいこなので、もう一度手を選んでください"
+  puts "0(グー)1(チョキ)2'(パー)3(戦わない)"
+else
+  break
+end
+end
+
+# ーーーーーあっち向いてほいの処理ーーーーーーー
+
+loop do
+  if @janken_type != 3
+    puts "あっち向いて"
+    puts "0(上)1(下)2(左)3(右)"
+
+    point_type = gets.to_i
+
+    puts "ショ！"
+
+    finger_type = ["上","下","左","右"]
+    i_hand = point_type
+    yo_hand = rand(0..3) #ランダムに出す
+
+    puts "あなた：#{finger_type[i_hand]}"
+    puts "相手：#{finger_type[yo_hand]}"
+
+    puts "ーーーーーーーーーーーーーーーーーーー"
+
+    if i_hand == yo_hand
+      puts "あなたの勝ちです！"
+      break  # 勝った場合、ループを抜ける
+    else
+      puts "あなたの負けです..."
+    end
   else
-    break
+    puts "じゃんけんを終了します"
+    break  # じゃんけんを終了する場合、ループを抜ける
   end
 end
+
